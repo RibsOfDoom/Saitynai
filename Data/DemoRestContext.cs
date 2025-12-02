d1fe35b2c226679339d918b2cf1c72d397f53aca
RibsOfDoom <162703272+RibsOfDoom@users.noreply.github.com>
2025-12-02 03:24:28 +0200

    changed rest

--- Data/DemoRestContext.cs
+++ Data/DemoRestContext.cs
@@ -1,26 +1,22 @@
 ﻿using L1_Zvejyba.Data.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using L1_Zvejyba.Data.Auth.Model;
-// For creating database open PowerShell and use Add-Migration
-// For uptading use Update-Database
 
 namespace L1_Zvejyba.Data
 {
     public class DemoRestContext : IdentityDbContext<User>
     {
-        public DbSet<Fish> Fish { get; set; }
-        public DbSet<City> Cities { get; set; }
-
-        public DbSet<Body> Bodies { get; set; }
-
-        public DbSet<Session> Sessions { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DemoRestContext(DbContextOptions<DemoRestContext> options)
+            : base(options)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=L1_Zvejyba");
         }
 
+        public DbSet<Fish> Fish { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Body> Bodies { get; set; }
+        public DbSet<Session> Sessions { get; set; }
 
+        // Remove OnConfiguring override entirely; use Program.cs for DB config
     }
 }
